3f0a1b144c9dda056ae2f0f8c8371996efc7cf78
Suneel Kumar Biyyapu <12517559+suneelbiyyapu@users.noreply.github.com>
2020-01-26 23:34:27 +0530

    Create ReplaceNULL.sql

---
+++ ReplaceNULL.sql
@@ -0,0 +1,15 @@
+-- This page will gives information about how to replace NULL in various ways
+
+-- Replacing NULL value using ISNULL() function
+SELECT ISNULL(NULL,'No Manager') as Manager
+
+-- Replacing NULL value using CASE Statement
+SELECT CASE 
+		 WHEN 'SUNEEL' IS NULL THEN 
+			'No Manager' 
+		ELSE 
+			'Manager' 
+			END as Manager
+
+-- Replacing NULL value using COALESCE() function
+SELECT COALESCE(NULL, 'No Manager') as Manager
